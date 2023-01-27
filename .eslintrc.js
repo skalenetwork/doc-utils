a79d6dea8d81b372d29ab94977a95a958972cb51
TheGreatAxios <sawyer@dirtroad.dev>
2023-01-27 12:36:41 -0800

    Add Linter Worfklow

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	"env": {
+		"browser": true,
+		"es2021": true
+	},
+	"extends": [
+		"plugin:@typescript-eslint/recommended",
+		"eslint-config-prettier",
+		"plugin:prettier/recommended"
+	],
+	"overrides": [
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"rules": {
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+	}
+};
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,5 +3,5 @@ import { sum } from "../src";
 describe("Unit Tests", () => {
   test("2 + 2 = 4", () => {
     expect(sum(2, 2)).toEqual(4);
-  })
-})
+  });
+});
