f18165e51581256b5d6143281961b9f3704c3d42
TheGreatAxios <sawyer@dirtroad.dev>
2023-02-21 20:02:38 -0800

    Add Lib Folder

---
+++ lib/cjs/types/jest.config.d.ts
@@ -0,0 +1,11 @@
+declare const _default: {
+    collectCoverage: boolean;
+    coverageDirectory: string;
+    coverageProvider: string;
+    testEnvironment: string;
+    transform: {
+        [x: string]: string | ["ts-jest", import("ts-jest").TsJestGlobalOptions] | ["ts-jest/legacy", import("ts-jest").TsJestGlobalOptions] | [string, Record<string, unknown>];
+    };
+};
+export default _default;
+//# sourceMappingURL=jest.config.d.ts.map
